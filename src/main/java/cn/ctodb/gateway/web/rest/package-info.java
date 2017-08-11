0a71e7be83e59e5bfdd0d12a33dcef431519ac2a
cc <lichaohn@163.com>
2017-08-11 13:25:36 +0800

    init

---
+++ src/main/java/cn/ctodb/gateway/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package cn.ctodb.gateway.web.rest;
---
+++ src/main/java/cn/ctodb/gateway/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package cn.ctodb.gateway.web.rest.vm;
